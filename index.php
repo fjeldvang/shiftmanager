96e895cc1b1107c1ee398fb8385bb82c519f28f9
Dennis Fjeldvang <dennisfjeldvang@gmail.com>
2021-04-19 14:14:29 +0200

    refactor stylesheet

--- index.php
+++ index.php
@@ -15,66 +15,10 @@ else
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
-<style>
-html *
-{
-   font-family: Arial !important;
-}
-table.calendar {
-	border-left: 1px solid #999;
-}
-tr.calendar-row {
-}
-td.calendar-day {
-	min-height: 80px;
-	font-size: 11px;
-	position: relative;
-	vertical-align: top;
-}
-* html div.calendar-day {
-	height: 80px;
-}
-td.calendar-day:hover {
-	background: #eceff5;
-}
-td.calendar-day-np {
-	background: #eee;
-	min-height: 80px;
-}
-* html div.calendar-day-np {
-	height: 80px;
-}
-td.calendar-day-head {
-	background: #ccc;
-	font-weight: bold;
-	text-align: center;
-	width: 120px;
-	padding: 5px;
-	border-bottom: 1px solid #999;
-	border-top: 1px solid #999;
-	border-right: 1px solid #999;
-}
-div.day-number {
-	background: #999;
-	padding: 5px;
-	color: #fff;
-	font-weight: bold;
-	float: right;
-	margin: -5px -5px 0 0;
-	width: 20px;
-	text-align: center;
-}
-td.calendar-day, td.calendar-day-np {
-	width: 120px;
-	padding: 5px;
-	border-bottom: 1px solid #999;
-	border-right: 1px solid #999;
-}
-</style>
+<link rel="stylesheet" href="stylesheet.css">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Shift calendar</title>
+<title>Shift kalender</title>
 <link href="jquery-ui.css" rel="stylesheet">
 <script src="jquery-1.10.2.js"></script>
 <script src="jquery-ui.js"></script>
@@ -101,7 +45,8 @@ td.calendar-day, td.calendar-day-np {
         $( "#from" ).datepicker( "option", "maxDate", selectedDate );
       }
     });
-  });  </script>
+  });  
+</script>
 </head>
 
 <body>
