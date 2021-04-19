bce13013207e2ea257a3a22dfc07924ee1ca3c5b
Dennis Fjeldvang <dennisfjeldvang@gmail.com>
2021-04-19 12:14:05 +0200

    update install.php

--- install.php
+++ install.php
@@ -7,24 +7,52 @@
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
-	
+	$tablename = "user";
 	// sql to create table
 	$sql = "CREATE TABLE $tablename (
-		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		userID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 		name VARCHAR(50) NOT NULL,
-		phone VARCHAR(20) NOT NULL,
-		item VARCHAR(20) NOT NULL,
-		start_day INT(11),
-		end_day INT(11),
-		start_time INT(11),
-		end_time INT(11),
-		canceled INT(1)
+		username VARCHAR(45) NOT NULL,
+		password VARCHAR(45) NOT NULL,
+		phone INT(11) NOT NULL
 	)";
 	
 	if (mysqli_query($conn, $sql)) {
 		echo "Table " . $tablename . " created successfully";
 	} else {
 		echo "Error creating table: " . mysqli_error($conn);
+	}
+
+	$tablename = "shifts";
+
+	$sql = "CREATE TABLE $tablename (
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	start_day INT(11),
+	end_day INT(11),
+	start_time INT(11),
+	end_time INT(11),
+	canceled INT(1),
+	userID int(11) UNSIGNED NOT NULL,
+	FOREIGN KEY (userID) REFERENCES user(userID)
+	)";
+
+	if (mysqli_query($conn, $sql)) {
+		echo "Table " . $tablename . " created successfully";
+	} else {
+		echo "Error creating table: " . mysqli_error($conn);
+	}
+
+	$tablename = "isadmin";
+
+	$sql = "CREATE TABLE $tablename(
+	userID int(11) UNSIGNED NOT NULL,
+	FOREIGN KEY (userID) REFERENCES user(userID)
+	)";
+
+	if (mysqli_query($conn, $sql)) {
+	echo "Table " . $tablename . " created successfully";
+	} else {
+	echo "Error creating table: " . mysqli_error($conn);
 	}
 	
 	mysqli_close($conn);
