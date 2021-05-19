61914453c66dc1edde71bbdf040b8fdf51ddc166
Dennis F <dennisfj@online.no>
2021-05-19 18:22:13 +0200

    change datepicker

--- javascript/datepicker.js
+++ javascript/datepicker.js
@@ -3,7 +3,7 @@ $(function() {
     $( "#from" ).datepicker({
       defaultDate: "+0w",
       changeMonth: true,
-      numberOfMonths: 3,
+      numberOfMonths: 2,
       onClose: function( selectedDate ) {
         $( "#to" ).datepicker( "option", "minDate", selectedDate );
       }
@@ -12,7 +12,7 @@ $(function() {
       defaultDate: "+0w",
 	  regional: "no",
       changeMonth: true,
-      numberOfMonths: 3,
+      numberOfMonths: 2,
       onClose: function( selectedDate ) {
         $( "#from" ).datepicker( "option", "maxDate", selectedDate );
       }
